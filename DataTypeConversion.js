b87d7a5cf897239da5cc5c30e26e228bcea6af3f
Aditya <adityakumar64064@gmail.com>
2026-03-26 14:56:49 +0530

    Learnt a bit about conversions

---
+++ DataTypeConversion.js
@@ -0,0 +1,14 @@
+//Conversion and Operations
+let score = undefined
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber) 
+//Number(example: 33) converted to Number: easily converted and printed
+//If the element being converted is not a number(example: 123abc), it shoqs NaN(Not a number) when we try to print it
+//In case of a Boolean value, 1 is printed for true and 0 for false
+//For null, we get 0
+//For undefined, we get NaN again
+/**Type of NaN: Number**/
+//Conversion to Boolean:1-true, 0-false, empty-false, string-true
+
+
